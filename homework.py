5e24ff4c420afe67b2c00efd5a6e41bd64f8bebe
Samandar Nemataliyev <samandarnemataliyev@gmail.com>
2024-12-17 08:54:14 +0500

    initial commit

---
+++ homework.py
@@ -0,0 +1,82 @@
+from random import randint
+import os
+os.system("clear")
+
+def random_array(k):
+    array = []
+    for _ in range(k):
+        array.append(randint(10, 100))
+    return array
+
+""" Series30 -> n, k natural sonlari va n ta butun sondan iborat k ta to'plam berilgan. 
+Har bir to'plam uchun elementlari yig'indisini chiqaruvchi programma tuzilsin."""
+def func30(n, k):
+    pass
+
+
+""" Series31 -> n, k natural sonlari va n ta butun sondan iborat k ta to'plam berilgan. 
+2 soni bor bo'lgan to'plamlar sonini chiqaruvchi programma tuzilsin."""
+def func31(n, k):
+    pass
+
+
+""" Series32 -> n, k natural sonlari va n ta butun sondan iborat k ta to'plam berilgan. 
+Har bir to'plamdagi birinchi uchragan 2 soni turgan nomerni chiqaruvchi programma tuzilsin. 
+Agar to'plamda 2 soni bo'lmasa, 0 chiqarilsin."""
+def func32(n, k):
+    pass
+
+
+""" Series33 -> n, k natural sonlari va n ta butun sondan iborat k ta to'plam berilgan. 
+Har bir to'plamdagi oxirgi 2 soni turgan nomerni chiqaruvchi programma tuzilsin. 
+Agar 2 soni to'plamda bo'lmasa, 0 chiqarilsin."""
+def func33(n, k):
+    pass
+
+
+""" Series34 -> n, k natural sonlari va n ta butun sondan iborat k ta to'plam berilgan. 
+Har bir to'plam uchun quyidagi vazifa bajarilsin. Agar to'plamda 2 soni bo'lsa, to'plam elementlari yig'indisi chiqarilsin,
+aks holda 0 chiqaruvchi programma tuzilsin."""
+def func34(n, k):
+    pass
+
+
+""" Series35 -> k natural soni v ak ta to'plam berilgan. Har bir to'plam elementlarini kiritish 0 bilan tugatiladi.
+Har bir to'plam uchun elementlar sonini chiqaruvchi programma tuzilsin."""
+def func35(k):
+    pass
+
+
+""" Series36 -> k natural soni va k ta to'plam berilgan. Har bir to'plam elementlarini kiritish 0 bilan tugatiladi.
+Har bir to'plam kamida 2 ta elementga ega. Elementlari o'suvchi bo'lgan to'plamlar sonini chiqaruvchi programma tuzilsin."""
+def func36(k):
+    pass
+
+
+""" Series37 -> k natural soni va k ta to'plam berilgan. Har bir to'plam elementlarini kiritish 0 bilan tugatiladi. 
+Har bir to'plam kamida 2 ta elementga ega. Elementlari o'suvchi yoki kamayuvchi bo'lgan to'plamlar sonini chiqaruvchi programma tuzilsin."""
+def func37(k):
+    pass
+
+
+""" Series38 -> k natural soni va k ta to'plam berilgan. Har bir to'plam elementlarini kiritish 0 bilan tugatiladi. 
+Har bir to'plam kamida 2 ta elementga ega. Har bir to'plam uchun quyidagi vazifalar bajarilsin: Agar to'plam elementlari 
+o'suvchi bo'lsa 1; kamayuvchi bo'lsa -1; o'suvchi ham kamayuvchi ham bo'lmasa 0 chiqaruvchi programma tuzilsin."""
+def func38(k):
+    pass
+
+
+""" Series39 -> k natural soni va k ta to'plam berilgan. Har bir to'plam elementlarini kiritish 0 bilan tugariladi. 
+Har bir to'plam kamida 3 ga elementga ega. Arrasimon to'plamlar sonini chiqaruvchi programma tuzilsin. (Series23 masalaga qarang)"""
+def func39(k):
+    pass
+
+
+""" Series40 -> k natural soni va k ta to'plam berilgan. Har bir to'plam elmentlarini kiritish 0 bilan tugatiladi. 
+Har bir to'plam kamida 3 ta elementga ega. Har bir to'plam uchun quyidagi vazifalar bajarilsin: 
+Agar to'plam elementlari arrasimon bo'lsa to'plam elementlari sonini chiqaruvchi;
+aks holda qonuniyatni buzgan element tartib raqami chiqarilsin."""
+def func40(k):
+    pass
+
+
